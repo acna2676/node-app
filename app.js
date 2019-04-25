44997d90e2436bac7cdd4878d068acc3c1214fa5
中石涼介 <ryosuke@home.local>
2019-04-25 21:24:37 +0900

    p156まで

--- app.js
+++ app.js
@@ -37,17 +37,44 @@ function getFromClient(request,response){
     }
 }
 
-var data = {
-    'Taro':'09-999-999',
-    'Hanako':'080-000-000'
+// var data = {
+//     'Taro':'09-999-999',
+//     'Hanako':'080-000-000'
+// };
+
+var data2 = {
+    'Taro':['@', '09-999-999'],
+    'Hanako':['@@','080-000-000']
 };
 
+var data = {msg:'no message...'};
 function response_index(request, response){
+    if(request.method == 'POST'){
+        var body = '';
+
+        request.on('data', (data) => {
+            body += data;
+    });
+
+    request.on('end',() => {
+        data = qs.parse(body);
+        setCookie('msg', data.msg, response);
+        write_index(request, response);
+    });
+}else{
+    write_index(request, response);
+}
+}
+
+
+function write_index(request, response){
         var msg = "message"
+        var cookie_data = getCookie('msg', request);
         var content = ejs.render(index_page, {
             title:"Index",
             content:msg,
-            data:data
+            data:data,
+            cookie_data:cookie_data
         });
         response.writeHead(200, {'Content-Type': 'text/html'});
         response.write(content);
@@ -68,7 +95,9 @@ function response_index(request, response){
             msg += post_data.msg + '!!';
         var content = ejs.render(other_page, {
             title:"Other",
-            content:msg
+            content:msg,
+            data:data2,
+            filename:'data_item'
             });
         response.writeHead(200, {'Content-Type': 'text/html'});
         response.write(content);
@@ -86,4 +115,21 @@ function response_index(request, response){
         response.write(style_css);
         response.end();
     }
+}
+
+function setCookie(key, value, response){
+    var cookie = escape(value);
+    response.setHeader('Set-Cookie', [key + '=' + cookie]);
+}
+
+function getCookie(key, request){
+    var cookie_data = request.headers.cookie != undefined ? request.headers.cookie : '';
+    var data = cookie_data.split(';');
+    for(var i in data){
+        if (data[i].trim().startsWith(key + '=')){
+            var result = data[i].trim().substring(key.length + 1);
+            return unescape(result);
+        }
+    }
+    return '';
 }
